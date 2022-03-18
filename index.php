126d6e8cbf72e40db9e4631b6919b21521ee7e7e
Pixeels <quintero.archie.o@gmail.com>
2022-03-18 11:56:54 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <title>United Nation World Peace</title>
+    <link href="bootstrap-5.1.3-dist\css\bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap-5.1.3-dist\js\bootstrap.min.js"></script>
+    <link href="assets\homepage\logo.png" rel="icon">
+
+</head>
+
+<body>
+    <div class="container-fluid m-0 p-0">
+        <img src="assets\homepage\header-rec.png" width="100%">
+        <img class="position-absolute top-0 start-0" src="assets\homepage\header-logobg.png" width="50%">
+        <img class="position-absolute top-10 start-0 w-sm" src="assets\homepage\main-logo.png" height="25%">
+
+        <div class="nav justify-content-end" style="margin-top:-12%; color:white">
+            <ul class="nav">
+                <li class="nav-item" style="color: white;">
+                    <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">NEWS AND UPDATE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">AMBASSADORS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">CONFERENCES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">STRUCTURE MEMBERS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ABOUT US</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    </div>
+</body>
+
+</html>
