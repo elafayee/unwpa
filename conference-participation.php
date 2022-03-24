f68659c6f9acaf0966e2ffa65d13b9d341efeafd
= <=>
2022-03-25 02:34:59 +0800

    update for conference participation

---
+++ conference-participation.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>United Nation World Peace</title>
+    <link href="bootstrap-5.1.3-dist\css\bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap-5.1.3-dist\js\bootstrap.min.js"></script>
+    <link href="assets\homepage\logo.png" rel="icon">
+    <link rel="stylesheet" href="scss\style.css">
+</head>
+<body>
+  <!-- Header Section -->
+  <?php include 'includes\header.php' ?>
+
+  <!-- Footer Section -->
+  <?php include 'includes\footer.php' ?>
+</body>
+</html>
