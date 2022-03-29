c763e6926e6a9bd00e8a712bb9521f9456fd191c
= <=>
2022-03-29 12:45:31 +0800

    update for header

--- index.php
+++ index.php
@@ -7,48 +7,12 @@
   <link href="assets\homepage\logo.png" rel="icon">
   <link rel="stylesheet" href="scss\style.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
   <!-- Header Section -->
-  <div class="container-fluid m-0 p-0 header-bg">
-    <div class="row mx-0">
-      <div class="col-sm-4 logo-bg mb-2">
-        <img src="assets\homepage\main-logo.png" alt="" srcset="" class="w-75 mt-2">
-      </div>
-      <div class="col-sm-8 h-50 d-flex mt-5">
-        <nav class="navbar navbar-expand-lg navbar-light align-self-end mt-5">
-          <a class="navbar-brand" href="#" disabled></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ">
-              <li class="nav-item">
-                <a class="nav-link active text-light fw-bold mx-1" aria-current="page" href="#">HOME</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light fw-bold mx-1" href="#">NEWS AND UPDATES</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light fw-bold mx-1" href="#">AMBASSADORS</a>
-              </li>
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link text-light fw-bold  mx-1" aria-current="page" href="#">CONFERENCES</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link text-light fw-bold mx-1" href="#">STRUCTURE MEMBERS</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link text-light fw-bold mx-1" href="#">ABOUT US</a>
-                </li>
-              </ul>
-          </div>
-        </nav>
-      </div>
-    </div>
-  </div>
+  <?php include 'includes\header.php' ?>
 
   <!-- Carousel Section -->
   <div class="container-fluid m-0 p-0 pageCarousel">
