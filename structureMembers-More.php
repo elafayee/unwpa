a5db6da645241ea51fd8e8317157f09af3ff71f1
unknown <louiejaysalvana@gmail.com>
2022-04-06 17:05:49 +0800

    structure members

---
+++ structureMembers-More.php
@@ -0,0 +1,64 @@
+<html lang="en">
+<head>
+<title>Structure Members Page</title>
+    <link href="bootstrap-5.1.3-dist\css\bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap-5.1.3-dist\js\bootstrap.min.js"></script>
+    <link href="assets\homepage\logo.png" rel="icon">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+
+<style>
+    body {
+        font-family: 'Poppins';
+        font-family: 20px;
+        color: white;
+    }
+</style>
+</head>
+
+<body>
+     <!-- Header Section -->
+  <?php include 'includes\header.php' ?>
+  <!-- Carousel Section -->
+
+  <!-- BODY CONTENT -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12 mt-5">
+            <h1 class="fw-bolder text-center text-dark">UNWPA MEMBER</h1>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row mb-5">
+            <div class="col-sm-12  d-flex justify-content-center">
+                <div class="card mb-3 w-100 mx-5 mt-5 p-5 bg-primary-cardbackground shadow">
+                    <div class="row g-0 p-2">
+                        <div class="col-md-4 mx-auto my-auto">
+                            <img src="assets\stucture members\10.png" class="img-fluid-profile-more mx-auto my-auto d-flex ">
+                            <div class="parallelogram d-flex px-3 mx-auto">
+                                <div class="vr my-auto"></div>
+                                <h6 class="text-white-fusao fw-bold my-auto">KATSUHIRO NISHINO</h6>
+                            </div>
+                            <div class="d-flex justify-content-center px-2">
+                                <h5 class="my-auto mt-1 mx-auto text-center text-white">Representative of International Security</h5>
+                            </div>
+
+                        </div>
+                        <!-- <div class="parallelogram d-flex"><div class="vr"></div><h6 class="text-white ">FUSAO KITAGAWA</h6></div> -->
+                        <div class="col-md-8 p-2 pt-2">
+                            <div class="card-body-content justify-content-around fs-5">
+                                <p class="card-text ">All humankind is equal. “Sunlight / Moonlight” is a light that all people can enjoy equally regardless of where they go in the world. However, the “light of security and safety” is still enjoyed only by the people of a very limited number of countries in the world. </p>
+                                <p class="card-text">There are many issues to be solved that are far from human rights, such as sad mayhem and war, the gap between rich and poor, human trafficking, abuse of vulnerable women and children, and resolution of vulnerable people by violence.</small></p>
+                                <p>The current state of the world. The lowest right of a person, “light of security and safety,” is given to as many people as possible as soon as possible, and “eat a full meal” and “sleep with peace of mind.” I strongly hope that it will be a reality.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+  <!-- Footer Section -->
+  <?php include 'includes\footer.php' ?>
+</body>
+</html>
